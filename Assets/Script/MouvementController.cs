270a1bc00e6bc25d63a3f5c11b8747830164526e
Jonathan Berube <jonathan_92222@hotmail.com>
2019-01-05 12:45:07 -0500

    Basic movement mechanic + Player controller

---
+++ Assets/Script/MouvementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementController : MonoBehaviour
+{
+
+    public float speed = 1;
+    private Rigidbody2D rb2d;
+
+
+    // Start is called before the first frame update
+    virtual protected void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    protected void Move(Vector2 movement)
+    {
+        rb2d.AddForce(movement * speed);
+    }
+
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MouvementController
+{
+    // Start is called before the first frame update
+    override protected void Start()
+    {
+        base.Start(); // Get rg2b component 
+    }
+
+    private void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        Move(movement);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
