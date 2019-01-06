a888cd00a7c8f3eb04b495e739096731a5bbb847
LuadenGames <luadengames@gmail.com>
2019-01-06 22:51:26 +0100

    Camera Script
    Added and tested camera script

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float stretchTime;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        RubberCamera();
+    }
+
+    void RubberCamera()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+            player.transform.position + offset, stretchTime * Time.deltaTime);
+    }
+}
